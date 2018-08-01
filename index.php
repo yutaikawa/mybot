9f96518e2bce16a3196fd45985d37e37b7642d05
yutaikawa <ikawa@maxim-jp.net>
2018-08-01 17:47:58 +0900

    first

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+// ライブラリを読み込み
+require_once(__DIR__ . '../line-bot-sdk-php/vendor/autoload.php');
+
+// POSTメソッドで渡される値の取得
+$inputString = file_get_contents('php://input');
+error_log($inputString);
