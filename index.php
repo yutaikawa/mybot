e91bf5dbc0ada8a0efc273a9e3146be4a1da087c
yutaikawa <ikawa@maxim-jp.net>
2018-08-05 15:03:00 +0900

    単一メッセージの送信

--- index.php
+++ index.php
@@ -3,6 +3,15 @@
 // ライブラリを読み込み
 require_once(__DIR__ . '/line-bot-sdk-php/vendor/autoload.php');
 
-// POSTメソッドで渡される値の取得
-$inputString = file_get_contents('php://input');
-error_log($inputString);
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+$bot = new LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+// 署名を取得
+$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+// 署名のチェック
+$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+
+// 各イベントをループで処理
+foreach ($events as $event) {
+	// テキストを返信
+	$bot->replyText($event->getReplyToken(), 'TextMessage');
+}
